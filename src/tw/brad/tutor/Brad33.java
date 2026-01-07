1a09f40f300059517a7e5366f7e9932b4ddfc789
JackShiao <shiaojackie0309@gmail.com>
2026-01-07 16:29:19 +0800

    寫完1A2B(尚未改良完善)，學完SET

---
+++ src/tw/brad/tutor/Brad33.java
@@ -0,0 +1,18 @@
+package tw.brad.tutor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Brad33 {
+	public static void main(String[] args) {
+		Set<Integer> lottery = new TreeSet<>();
+		while (lottery.size() < 6) {
+			lottery.add((int)(Math.random()*49+1));
+		}
+		System.out.println(lottery);
+		for (Integer i : lottery) {
+			System.out.println(i);
+		}
+	}
+}
